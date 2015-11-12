6a80dc2f5cb2e60cf8fd7776ca32e5abd48f0131
Ricky Elrod <ricky@elrod.me>
2015-11-12 12:17:42 -0500

    Move mvConfig into Flight.NBP3
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/KD8ZRC/Flight/NBP3/Main.hs
+++ src/KD8ZRC/Flight/NBP3/Main.hs
@@ -1,11 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
 -- | This is the runner program for NBP-3. It ties together things from mapview
 -- (the core library), helper functions from other KD8ZRC.Flight.NBP3.* modules,
 -- and so on, into one program.
 module Main where
 
---import KD8ZRC.Flight.NBP3.CRC
---import KD8ZRC.Flight.NBP3.Parser
---import KD8ZRC.Flight.NBP3.Types
+import KD8ZRC.Flight.NBP3.Parser
+import KD8ZRC.Flight.NBP3.Types
+import KD8ZRC.Mapview.Types
+import KD8ZRC.Mapview.Utility.Downlink
+import KD8ZRC.Mapview.Utility.Logging
+
+mvConfig :: MapviewConfig TelemetryLine
+mvConfig = MapviewConfig {
+    _mvParser = parser
+  , _mvDownlinkSpawn = modemStdout "minimodem" ["-r", "-q", "rtty", "-S", "700", "-M", "870"]
+  , _mvPacketLineCallback = [logRawPacketStdout]
+}
 
 main :: IO ()
 main = error "Go NBP3!"
