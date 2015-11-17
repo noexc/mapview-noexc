0e1e5042c353f59dbb9d4133ab6b7aba284c6ba1
Ricky Elrod <ricky@elrod.me>
2015-11-17 01:36:20 -0500

    More likely what threading will look like.
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/KD8ZRC/Flight/NBP3/Main.hs
+++ src/KD8ZRC/Flight/NBP3/Main.hs
@@ -4,6 +4,8 @@
 -- and so on, into one program.
 module Main where
 
+import Control.Concurrent
+import Control.Monad.IO.Class
 import KD8ZRC.Flight.NBP3.Parser
 import KD8ZRC.Flight.NBP3.Types
 import KD8ZRC.Mapview.Execute
@@ -11,17 +13,39 @@ import KD8ZRC.Mapview.Types
 import KD8ZRC.Mapview.Utility.Downlink
 import KD8ZRC.Mapview.Utility.Logging
 
-mvConfig :: MapviewConfig TelemetryLine
-mvConfig = MapviewConfig {
+-- | A simple example to show how a logger thread might work.
+data Logger = Logger (MVar String)
+
+initLogger :: IO Logger
+initLogger = do
+  m <- newEmptyMVar
+  let l = Logger m
+  _ <- forkIO (logVia m)
+  return l
+
+logVia :: MVar String -> IO ()
+logVia v = do
+  str <- takeMVar v
+  putStrLn $ "Printed from another thread: " ++ str
+  logVia v
+
+logPacket :: Logger -> PacketLineCallback t
+logPacket (Logger m) = PacketLineCallback (liftIO . putMVar m)
+
+mvConfig :: Logger -> MapviewConfig TelemetryLine
+mvConfig logger = MapviewConfig {
     _mvParser = parser
   , _mvDownlinkSpawn =
       modemStdout "minimodem" ["-r", "-q", "rtty", "-S", "700", "-M", "870"]
   , _mvPacketLineCallback =
       [ logRawPacketFile "/tmp/nbp3.log"
       , logRawPacketStdout
+      , logPacket logger
       ]
   , _mvParsedPacketCallback = logParsedPacketStdout
 }
 
 main :: IO ()
-main = mapview mvConfig
+main = do
+  logger <- initLogger
+  mapview (mvConfig logger)
