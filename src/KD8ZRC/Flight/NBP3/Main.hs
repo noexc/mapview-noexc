cd60397456953f81792ca47d9c88acd434ae6889
Ricky Elrod <ricky@elrod.me>
2016-01-04 08:39:49 -0500

    Make use of mapview-websockets work
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/KD8ZRC/Flight/NBP3/Main.hs
+++ src/KD8ZRC/Flight/NBP3/Main.hs
@@ -5,29 +5,33 @@
 module Main where
 
 import Control.Concurrent
-import Control.Monad.IO.Class
+import qualified Control.Concurrent.Chan as Chan
+import qualified Data.Text as T
+
 import KD8ZRC.Flight.NBP3.Parser
 import KD8ZRC.Flight.NBP3.Types
 import KD8ZRC.Mapview.Execute
 import KD8ZRC.Mapview.Types
+import KD8ZRC.Mapview.Utility.Concurrent
 import KD8ZRC.Mapview.Utility.Downlink
 import KD8ZRC.Mapview.Utility.Logging
 import KD8ZRC.Mapview.Utility.Websocket
 
-mvConfig :: WebsocketServer -> MapviewConfig TelemetryLine
-mvConfig _ws = MapviewConfig {
+mvConfig :: Chan.Chan T.Text -> MapviewConfig TelemetryLine
+mvConfig _ch = MapviewConfig {
     _mvParser = parser
   , _mvDownlinkSpawn =
       modemStdout "minimodem" ["-r", "-q", "rtty", "-S", "700", "-M", "870"]
   , _mvPacketLineCallback =
       [ logRawPacketFile "/tmp/nbp3.log"
       , logRawPacketStdout
-      , broadcastRaw _ws
+      , writeChanRaw _ch
       ]
   , _mvParsedPacketCallback = logParsedPacketStdout
 }
 
 main :: IO ()
 main = do
-  ws <- initWebsocketServer "127.0.0.1" 8181
-  mapview (mvConfig ws)
+  rawChan <- Chan.newChan
+  _ <- forkIO $ initWebsocketServer rawChan "127.0.0.1" 8181
+  mapview (mvConfig rawChan)
